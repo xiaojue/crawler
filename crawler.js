e13cd9dca4dacc9f51e7222ce367bf872b033718
xiaojue <designsor@gmail.com>
2012-05-03 06:28:42 +0400

    add events end and collect and error

--- crawler.js
+++ crawler.js
@@ -4,56 +4,42 @@
  */
 
 var http = require('http'),
+events = require('events').EventEmitter,
 Iconv = require('iconv').Iconv,
 gb2312_to_utf8_iconv = new Iconv('GBK', 'UTF-8'),
 htmlparser = require('htmlparser'),
 select = require('soupselect').select;
 
-function mix(o, s) {
-	for (var i in s) {
-		o[i] = s[i];
+var crawler = function(hosts, size, ToGBK) {
+	/*
+	baidu: {
+		host: 'www.baidu.com',
+		path: '/s?wd=' + key,
+		port: 80,
+		method: 'get'
+	},
+	google: {
+		host: 'www.google.com',
+		path: '/search?hl=zh-CN&oe=utf-8&ie=utf-8&q=' + key,
+		port: 80,
+		method: 'get'
 	}
-}
-
-var crawler = function(param) {
-	this.param = {
-		target: 'baidu',
-		maxsize: 10,
-		key: '测试',
-		dir: './temp'
-	};
+    */
+	this.hosts = hosts;
+	this.ToGBK = ToGBK;
+	this.size = size || 10;
 	this.len = 0;
-    this.pool = [];
-	mix(this.param, param);
+	events.call(this);
 };
 
-crawler.prototype = {
-	constructor: crawler,
-	collector: function(key) {
-		var collection = {
-			baidu: {
-				host: 'www.baidu.com',
-				path: '/s?wd=' + key,
-				port: 80,
-				method: 'get'
-			},
-			google: {
-				host: 'www.google.com',
-				path: '/search?hl=zh-CN&oe=utf-8&ie=utf-8&q=' + key,
-				port: 80,
-				method: 'get'
-			}
-		};
-		var codingToGBK = {
-			baidu: true,
-			google: false
-		};
-		return [collection[this.param.target], codingToGBK[this.param.target]];
+crawler.prototype = Object.create(events.prototype, {
+	constructor: {
+		value: crawler,
+		enumerable: false
 	},
 	open: function(callback) {
-		var param = this.collector(this.param.key),
-		pageStr = '',
-		request = http.get(param[0], function(res) {
+		var pageStr = '',
+		request = http.get(this.hosts, function(res) {
 			var buffers = [],
 			size = 0,
 			pageStr;
@@ -68,7 +54,7 @@ crawler.prototype = {
 					buffers[i].copy(buffer, pos);
 					pos += buffers[i].length;
 				}
-				if (param[1]) {
+				if (this.ToGBK) {
 					var utf8_buffer = gb2312_to_utf8_iconv.convert(buffer);
 					pageStr = utf8_buffer.toString();
 				} else {
@@ -81,28 +67,34 @@ crawler.prototype = {
 			callback(e);
 		});
 	},
-	hrefcollect: function(callback) {
-        var self = this;
+	collect: function(callback) {
+		var self = this;
 		this.open(function(err, str) {
-			var handler = new htmlparser.DefaultHandler(function(err, dom) {
-				if (!err) {
-                    var pool = [];
-					select(dom, 'a').forEach(function(ele) {
-                        pool.push(ele.attribs.href);                        
-					});
-                    callback(null,pool);
-				}else{
-                    callback(err);    
-                }
-			});
-			var parser = new htmlparser.Parser(handler);
-			parser.parseComplete(str);
+			if (!err) {
+				var handler = new htmlparser.DefaultHandler(function(err, dom) {
+					if (err) self.emit('collect', err);
+					else self.emit('error', err);
+					if (callback) callback(err, dom);
+				});
+				var parser = new htmlparser.Parser(handler);
+				parser.parseComplete(str);
+			} else {
+				self.emit('error', err);
+			}
 		});
+	},
+	start: function() {
+		var self = this;
+		for (var i = 0; i < this.size; i++) { (function(i) {
+				this.collect(function() {
+					self.len++;
+					if (i == self.len) {
+						self.emit('end');
+						self.len = 0;
+					}
+				});
+			})(i);
+		}
 	}
-};
-
-var test = new crawler();
-test.hrefcollect(function(err,pool){
-    console.log(pool);
 });
 
